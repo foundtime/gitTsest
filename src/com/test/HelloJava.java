a3e7fdb658b09cea6ddc9b54b27ced612ba99cf3
foundtime <sky@DESKTOP-JAKNR5U>
2018-06-09 17:43:06 +0800

    分支

--- src/com/test/HelloJava.java
+++ src/com/test/HelloJava.java
@@ -4,6 +4,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("欢迎来到java世界,我的git测试!");
 		System.out.println("再次生成!");
+		System.out.println("我是分支!");
 	}
 
 }
